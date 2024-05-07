b8a729201964be0cb6f7a5a5f1f711c358b3e6a4
cairomedeiros <cairomedeiros13@gmail.com>
2024-05-07 17:38:44 -0300

    starting a server in go

--- cmd/todo-list-api/main.go
+++ cmd/todo-list-api/main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to my api!")
+	})
+
+	fmt.Println("Server started in http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
