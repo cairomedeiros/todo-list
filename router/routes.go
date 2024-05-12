066f066297529888091f3e6f9dafefbc8f84f15d
cairomedeiros <cairomedeiros13@gmail.com>
2024-05-12 20:58:02 -0300

    add separate files to handlers

--- router/routes.go
+++ router/routes.go
@@ -1,35 +1,14 @@
 package router
 
 import (
-	"net/http"
-
+	"github.com/cairomedeiros/todo-list/handler"
 	"github.com/gorilla/mux"
 )
 
 func initializeRoutes(r *mux.Router) {
-	r.HandleFunc("/createtodo", CreateToDoHandler) //POST
-	r.HandleFunc("/listAll", ListAllHandler)       //GET
-	r.HandleFunc("/{id}", GetByIdHandler)          //GET
-	r.HandleFunc("/{id}", UpdateToDoHandler)       //PUT
-	r.HandleFunc("/{id}", DeleteToDoHandler)       //DELETE
-}
-
-func CreateToDoHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Create new ToDO"))
-}
-
-func ListAllHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("List all ToDo"))
-}
-
-func GetByIdHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Get by ToDo by id"))
-}
-
-func UpdateToDoHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Update ToDo"))
-}
-
-func DeleteToDoHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Delete this ToDo"))
+	r.HandleFunc("/createtodo", handler.CreateToDoHandler).Methods("POST") //POST
+	r.HandleFunc("/listAll", handler.ListAllHandler).Methods("GET")        //GET
+	r.HandleFunc("/{id}", handler.GetByIdHandler).Methods("GET")           //GET
+	r.HandleFunc("/{id}", handler.UpdateToDoHandler).Methods("PUT")        //PUT
+	r.HandleFunc("/{id}", handler.DeleteToDoHandler).Methods("DELETE")     //DELETE
 }
