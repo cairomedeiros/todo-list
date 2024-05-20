a3e6935735372a2f9a2587ca3b7f466a05fba8f3
cairomedeiros <cairomedeiros13@gmail.com>
2024-05-20 08:05:55 -0300

    implementation of get by id method

--- handler/getById.go
+++ handler/getById.go
@@ -1,7 +1,30 @@
 package handler
 
-import "net/http"
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	"github.com/cairomedeiros/todo-list/schemas"
+	"github.com/gorilla/mux"
+	"gorm.io/gorm"
+)
 
 func GetByIdHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Get by ToDo by id"))
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	todo := schemas.ToDo{}
+
+	if err := db.First(&todo, id).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			http.Error(w, "Data not found", http.StatusNotFound)
+			return
+		}
+
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(todo)
 }
