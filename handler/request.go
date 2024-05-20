3037b55e1fd01262059324ac8c365edb1614648c
cairomedeiros <cairomedeiros13@gmail.com>
2024-05-20 08:05:23 -0300

    add id field to create todo request as primary key

--- handler/request.go
+++ handler/request.go
@@ -3,6 +3,7 @@ package handler
 import "time"
 
 type CreateToDoRequest struct {
+	Id           uint      `json:"id" gorm:"primaryKey"`
 	Title        string    `json:"title"`
 	Description  string    `json:"description"`
 	CreationDate time.Time `json:"creationDate"`
