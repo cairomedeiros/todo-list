704dbb4bc3fd63f7746ddd2b1a053b4cf9261a81
cairomedeiros <cairomedeiros13@gmail.com>
2024-05-21 09:22:14 -0300

    implementation of list all records

--- handler/listAll.go
+++ handler/listAll.go
@@ -1,7 +1,26 @@
 package handler
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/cairomedeiros/todo-list/schemas"
+)
 
 func ListAllHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("List all ToDo"))
+	var todos []schemas.ToDo
+
+	result := db.Find(&todos)
+
+	if result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+	}
+
+	log.Printf("%d records found.\n", result.RowsAffected)
+
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(todos); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
