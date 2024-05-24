942e7dc8c2c4376a915df148900078fd104d4ec1
cairomedeiros <cairomedeiros13@gmail.com>
2024-05-24 16:29:00 -0300

    adjust in routes path

--- router/routes.go
+++ router/routes.go
@@ -12,16 +12,16 @@ func initializeRoutes(r *mux.Router) {
 	handler.InitializeHandler()
 
 	//tasks
-	r.HandleFunc("/createTask", task.CreateTaskHandler).Methods("POST") //POST
-	r.HandleFunc("/listAll", task.ListAllHandler).Methods("GET")        //GET
-	r.HandleFunc("/{id}", task.GetByIdHandler).Methods("GET")           //GET
-	r.HandleFunc("/{id}", task.UpdateTaskHandler).Methods("PUT")        //PUT
-	r.HandleFunc("/{id}", task.DeleteTaskHandler).Methods("DELETE")     //DELETE
+	r.HandleFunc("/task/create", task.CreateTaskHandler).Methods("POST") //POST
+	r.HandleFunc("/task/listAll", task.ListAllHandler).Methods("GET")    //GET
+	r.HandleFunc("/task/{id}", task.GetByIdHandler).Methods("GET")       //GET
+	r.HandleFunc("/task/{id}", task.UpdateTaskHandler).Methods("PUT")    //PUT
+	r.HandleFunc("/task/{id}", task.DeleteTaskHandler).Methods("DELETE") //DELETE
 
 	//subTask
-	r.HandleFunc("/createSubTask", subTask.CreateSubTaskHandler).Methods("POST") //POST
-	r.HandleFunc("/{id}", subTask.UpdateSubTaskHandler).Methods("PUT")           //PUT
-	r.HandleFunc("/{id}", subTask.DeleteSubTaskHandler).Methods("DELETE")        //DELETE
-	r.HandleFunc("/listAll", subTask.ListAllHandler).Methods("GET")              //GET
-	r.HandleFunc("/{id}", subTask.GetByIdHandler).Methods("GET")                 //GET
+	r.HandleFunc("/subTask/create", subTask.CreateSubTaskHandler).Methods("POST") //POST
+	r.HandleFunc("/subTask/{id}", subTask.UpdateSubTaskHandler).Methods("PUT")    //PUT
+	r.HandleFunc("/subTask/{id}", subTask.DeleteSubTaskHandler).Methods("DELETE") //DELETE
+	r.HandleFunc("/subTask/listAll", subTask.ListAllHandler).Methods("GET")       //GET
+	r.HandleFunc("/subTask/{id}", subTask.GetByIdHandler).Methods("GET")          //GET
 }
