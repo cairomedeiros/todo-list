0667d010c22dc63a6d36c2e46ab4d003c57b5a2e
cairomedeiros <cairomedeiros13@gmail.com>
2024-05-28 22:53:36 -0300

    add TaskResponse to return in swagger

--- schemas/task.go
+++ schemas/task.go
@@ -14,3 +14,14 @@ type Task struct {
 	Completed   bool
 	SubTasks    *[]SubTask `gorm:"foreignKey:TaskID"`
 }
+
+type TaskResponse struct {
+	ID          uint       `json:"id"`
+	CreatedAt   time.Time  `json:"createdAt"`
+	UpdatedAt   time.Time  `json:"updatedAt"`
+	DeletedAt   time.Time  `json:"deteledAt,omitempty"`
+	Title       string     `json:"title"`
+	Description string     `json:"description"`
+	DueDate     *time.Time `json:"dueDate"`
+	Completed   bool       `json:"completed"`
+}
